ae4b2fcb55cade26f5327da6bb0baf721c208652
aldoughri <47716351+aldoughri@users.noreply.github.com>
2019-05-17 19:09:16 +0300

    Update category.php
    category create

---
+++ aletqan_project/api/category.php
@@ -0,0 +1,15 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+require_once(__DIR__ . '/../../../config.php'); 
+require_once($CFG->dirroot.'/course/externallib.php');
+$dbclass = new DBClass();
+$connection = $dbclass->getConnection();
+$external   =new core_course_external();
+$create_category=$external->create_categories($categories);
+$courses_opject=json_decode ( json_encode($create_category) );
+
