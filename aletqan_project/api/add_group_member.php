1be0da94c44125a1577711f20a15a2efc2f21a6f
aldoughri <47716351+aldoughri@users.noreply.github.com>
2019-05-17 22:43:53 +0300

    Create add_group_member.php

---
+++ aletqan_project/api/add_group_member.php
@@ -0,0 +1,15 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+require_once(__DIR__ . '/../../../config.php'); 
+require_once($CFG->dirroot.'/enrol/locallib.php');
+$dbclass = new DBClass();
+$connection = $dbclass->getConnection();
+$locallib   =new course_enrolment_manager($modelpage,$course);
+$add_user_to_group=$locallib-> add_user_to_group($user, $groupid);
+//$add_user_to_group_opject=json_decode ( json_encode($add_user_to_group) );
+//$remove_member= json_encode($add_user_to_groupr_opject );
