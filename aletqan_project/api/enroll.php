461ec5e30b1d768d24555023e1682823ee21fb9f
omar <khadamaljamyomar@gmail.com>
2019-05-18 05:18:01 +0300

    Completed enroll pre condition

--- aletqan_project/api/enroll.php
+++ aletqan_project/api/enroll.php
@@ -8,19 +8,25 @@ include_once '../table/Coursemoodle.php';
 include_once '../table/Courseinfo.php';
 include_once '../config/DBClass.php';
 include_once '../config/dbclassmoodel.php';
+require_once($CFG->dirroot . '/lib/grouplib.php');
 
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->dirroot . '/course/externallib.php');
 
 
 if (
-    !($srudent_id = empty($_POST['student_id'])) &&
+    !($user_id = empty($_POST['user_id'])) &&
     !($group_id = empty($_POST['group_id'])) &&
-    !($course_id = empty($_POST['course_id'])) 
-){ 
-    if(!is_enroll($course_id, $group_id)){
-        
-    }else {
-        //the user may existed in the course but he want to re-enroll in it
-    }
+    !($course_id = empty($_POST['course_id'])) &&
+    !($payment = empty($_POST['payment']))
+
+) {
+    if (user_in_moodle($user_id))
+        if (!is_enroll($course_id, $user_id)) {
+            if (!groups_is_member($group_id, $user_id))
+                if (course_have_group($course_id, $group_id))
+                    if (first_payment_condition($group_id, $payment))
+        } else {
+            //the user may existed in the course but he want to re-enroll in it
+        }
 }
--- aletqan_project/lib/enrollib.php
+++ aletqan_project/lib/enrollib.php
@@ -8,7 +8,7 @@ include_once '../table/Student_payment.php';
 include_once '../table/Real_cours.php';
 
 //api add first payment to student
-function first_payment($student_id, $group_id, $Quantity)
+function first_payment($student_id, $group_id, $payment)
 {
 
     $dbclass = new DBClass();
@@ -16,17 +16,17 @@ function first_payment($student_id, $group_id, $Quantity)
     $realcours = new Real_cours($connection);
     $stmt =    $realcours->select_group($group_id);
     if ($st = $stmt->fetch()) {
-        $realcours_opject = json_decode(json_encode($st));
-        $percent = $realcours_opject->price * 40 / 100;
-        if ($Quantity >= $percent) {
+        $realcours_object = json_decode(json_encode($st));
+        $percent = $realcours_object->price * 40 / 100;
+        if ($payment >= $percent) {
 
-            if ($Quantity < $realcours_opject->price) {
+            if ($payment < $realcours_object->price) {
 
                 $student_payment = new Student_payment($connection);
                 $student_payment->student_id = $student_id;
-                $student_payment->realcours_id = $realcours_opject->realcours_id;
+                $student_payment->realcours_id = $realcours_object->realcours_id;
                 $student_payment->date = date('Y-m-d');
-                $student_payment->amount = $Quantity;
+                $student_payment->amount = $payment;
 
                 if ($student_payment->create()) {
                     echo "payment was created";
@@ -36,7 +36,7 @@ function first_payment($student_id, $group_id, $Quantity)
                     return false;
                 }
             } else {
-                echo "The quantity is larger than the price of the course " . $realcours_opject->price;
+                echo "The quantity is larger than the price of the course " . $realcours_object->price;
                 return false;
             }
         } else {
@@ -67,85 +67,85 @@ function user_in_moodle($user_id)
     return ($DB->get_record('user', ['id' => $user_id])) != null;
 }
 
-function first_payment_condition($student_id, $group_id, $Quantity)
+function first_payment_condition($group_id, $payment)
 {
     $dbclass = new DBClass();
     $connection = $dbclass->getConnection();
     $realcours = new Real_cours($connection);
     $stmt =    $realcours->select_group($group_id);
     if ($st = $stmt->fetch()) {
-        $realcours_opject = json_decode(json_encode($st));
-        $percent = $realcours_opject->price * 40 / 100;
-        if ($Quantity >= $percent && $Quantity < $realcours_opject->price)
+        $realcours_object = json_decode(json_encode($st));
+        $percent = $realcours_object->price * 40 / 100;
+        if ($payment >= $percent && $payment < $realcours_object->price)
             return true;
     }
     return false;
 }
 
-    //api add payment 
-    function  add_payment($student_id, $group_id, $course_id, $Quantity)
-    {
-        if (is_enroll($course_id, $student_id)) {
-            if (groups_is_member($group_id, $student_id)) {
-                $realcours_opject =    select_real_course($group_id);
-                $Remaining_amount =    $realcours_opject->price - sum_payment_student_in_group($group_id, $student_id);
-                if ($Quantity < $Remaining_amount) {
-                    if ($Quantity > 0) {
-                        $dbclass = new DBClass();
-                        $connection = $dbclass->getConnection();
-                        $student_payment = new Student_payment($connection);
-                        $student_payment->student_id = $student_id;
-                        $student_payment->realcours_id = $group_id;
-                        $student_payment->date = date('Y-m-d');
-                        $student_payment->amount = $Quantity;
+//api add payment 
+function  add_payment($student_id, $group_id, $course_id, $payment)
+{
+    if (is_enroll($course_id, $student_id)) {
+        if (groups_is_member($group_id, $student_id)) {
+            $realcours_object =    select_real_course($group_id);
+            $Remaining_amount =    $realcours_object->price - sum_payment_student_in_group($group_id, $student_id);
+            if ($payment < $Remaining_amount) {
+                if ($payment > 0) {
+                    $dbclass = new DBClass();
+                    $connection = $dbclass->getConnection();
+                    $student_payment = new Student_payment($connection);
+                    $student_payment->student_id = $student_id;
+                    $student_payment->realcours_id = $group_id;
+                    $student_payment->date = date('Y-m-d');
+                    $student_payment->amount = $payment;
 
-                        if ($student_payment->create()) {
-                            echo "payment was created";
-                            return true;
-                        } else {
-                            echo "payment not created";
-                            return false;
-                        }
+                    if ($student_payment->create()) {
+                        echo "payment was created";
+                        return true;
                     } else {
-                        echo 'Quantity is not acceptable where remaining amount= ' . $Remaining_amount;
+                        echo "payment not created";
                         return false;
                     }
                 } else {
-                    echo 'The quantity is greater than the remaining amount  = ' . $Remaining_amount;
+                    echo 'Quantity is not acceptable where remaining amount= ' . $Remaining_amount;
                     return false;
                 }
             } else {
-                echo 'student not exist in group';
+                echo 'The quantity is greater than the remaining amount  = ' . $Remaining_amount;
                 return false;
             }
         } else {
-            echo 'student not enroll';
+            echo 'student not exist in group';
             return false;
         }
-    }
-
-    //return apject json to realcourse 
-    function select_real_course($group_id)
-    {
-
-        $dbclass = new DBClass();
-        $connection = $dbclass->getConnection();
-        $realcours = new Real_cours($connection);
-        $stmt =    $realcours->select_group($group_id);
-        if ($st = $stmt->fetch(2)) {
-            $realcours_opject = json_decode(json_encode($st));
-            return $realcours_opject;
-        }
+    } else {
+        echo 'student not enroll';
         return false;
     }
+}
 
-    //return int to  sum  payment student in realcourse'group' 
-    function sum_payment_student_in_group($group_id, $student_id)
-    {
-        $dbclass = new DBClass();
-        $connection = $dbclass->getConnection();
-        $payment = new Student_payment($connection);
-        $stmt = $payment->sum_payment_student_in_group($group_id, $student_id);
-        $st = $stmt->fetch(3);
-        return $st[0];
+//return apject json to realcourse 
+function select_real_course($group_id)
+{
+
+    $dbclass = new DBClass();
+    $connection = $dbclass->getConnection();
+    $realcours = new Real_cours($connection);
+    $stmt =    $realcours->select_group($group_id);
+    if ($st = $stmt->fetch(2)) {
+        $realcours_object = json_decode(json_encode($st));
+        return $realcours_object;
     }
+    return false;
+}
+
+//return int to  sum  payment student in realcourse'group' 
+function sum_payment_student_in_group($group_id, $student_id)
+{
+    $dbclass = new DBClass();
+    $connection = $dbclass->getConnection();
+    $payment = new Student_payment($connection);
+    $stmt = $payment->sum_payment_student_in_group($group_id, $student_id);
+    $st = $stmt->fetch(3);
+    return $st[0];
+}
