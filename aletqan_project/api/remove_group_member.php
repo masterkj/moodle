087c79984a3ca6cd29cba998027edef90e786951
basharhaidar <bashar.sy.1997@hotmail.com>
2019-05-20 05:24:21 +0300

    add api remove_group_member

--- aletqan_project/api/remove_group_member.php
+++ aletqan_project/api/remove_group_member.php
@@ -5,16 +5,22 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-require_once($CFG->dirroot.'/enrol/locallib.php');
-include_once '../table/groupmoodle.php';
-include_once '../table/Real_cours.php';
-include_once '../config/DBClass.php';
-$dbclass = new DBClass();
-$connection = $dbclass->getConnection();
-if(!empty($_POST['group_id']))
-    if(!empty($_POST['user_id'])){
-        $group_id=$_POST['group_id'];
-        $user_id=$_POST['user_id'];
+require_once __DIR__ . '/../../config.php';
+require_once $CFG->dirroot . '/group/lib.php';
+
+if (!empty($_POST['group_id']) &&
+    !empty($_POST['user_id'])
+) {
+    $group_id = $_POST['group_id'];
+    $user_id = $_POST['user_id'];
+
+    if(groups_remove_member( $group_id , $user_id )){
+        http_response_code(200);
+        echo json_encode(array("message" => "true"));
+
+    }else{
+        http_response_code(404);
+        echo json_encode(array("message" => "false"));
     }
-$locallib   =new course_enrolment_manager($modelpage,$course);
-$remove_member=$locallib-> remove_user_from_group($group_id, $user_id);
+}
+
