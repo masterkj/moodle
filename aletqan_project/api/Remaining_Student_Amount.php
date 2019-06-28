a0fa932d8b2f92c9a52ca8d69bcc5ee93fa63b4d
basharhaidar <bashar.sy.1997@hotmail.com>
2019-06-28 19:44:22 +0300

    Remaining_Student_Amount

--- aletqan_project/api/Remaining_Student_Amount.php
+++ aletqan_project/api/Remaining_Student_Amount.php
@@ -1,42 +1,22 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-//header("Access-Control-Allow-Methods: GET");
-
-include_once '../table/Real_cours.php';
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+include_once '../table/Student_payment.php';
 include_once '../config/DBClass.php';
+require_once __DIR__ . '/../../config.php';
+require_once $CFG->dirroot . '/course/externallib.php';
+
+
 
 $dbclass = new DBClass();
 $connection = $dbclass->getConnection();
-$realcours = new Real_cours($connection);
+$student_payment = new Student_payment($connection);
+$stmt = $student_payment->Remaining_Student_Amount($_POST['student_id']);
+$st = $stmt->fetchall(2);
 
-$stmt = $realcours->readcorsenotfinished();
-$count = $stmt->rowCount();
-if ($count > 0) {
-
-
-    $courses_not_finished = array();
-    $courses_not_finished["body"] = array();
-    $courses_not_finished["count"] = $count;
-
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-
-        extract($row);
-
-        $p  = array(
-            "realcours_id" => $realcours_id,
-            "cours_id" => $cours_id,
-            "Teacher_id" => $Teacher_id,
-            "start_date" => $start_date,
-            "end_date" => $end_date,
-            "price" => $price,
-            "attendance_days" => $attendance_days,
-        );
-
-        array_push($courses_not_finished["body"], $p);
-    }
-
-    echo json_encode($courses_not_finished);
-} else {
-
-    echo json_encode(array("bodyelse" => array(), "count" => 0));
-}
+$Remaining_Student_Amount = json_encode($st);
+echo $Remaining_Student_Amount;
--- aletqan_project/table/Real_cours.php
+++ aletqan_project/table/Real_cours.php
@@ -74,14 +74,6 @@ class Real_cours{
         return $stmt;
     }
 
-    public function read_Remaining_Student_Amount(){
-        $query = "SELECT realcours_id , cours_id ,group_id ,Teacher_id , start_date ,end_date,price,attendance_days  FROM " . $this->table_name . " where end_date >= CAST(CURRENT_TIMESTAMP AS DATE)";
-        $stmt = $this->connection->prepare($query);
-
-        $stmt->execute();
-
-        return $stmt;
-    }
 
 
     public function select_group($id_group){
