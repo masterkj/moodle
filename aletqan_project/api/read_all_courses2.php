91491f1dccfdb785f9a0a7649109e8eee5246af4
basharhaidar <bashar.sy.1997@hotmail.com>
2019-07-01 04:18:52 +0300

    add header to read all courses

--- aletqan_project/api/read_all_courses2.php
+++ aletqan_project/api/read_all_courses2.php
@@ -1,5 +1,10 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
 include_once '../table/Courseinfo.php';
 include_once '../config/DBClass.php';
 include_once '../config/dbclassmoodel.php';
