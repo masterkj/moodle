1b2391d38d6de17bc2f36c8564a35171de812573
basharhaidar <bashar.sy.1997@hotmail.com>
2019-07-03 01:11:21 +0300

    update enter username bdl id name

--- aletqan_project/api/enroll.php
+++ aletqan_project/api/enroll.php
@@ -4,24 +4,28 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-include_once '../table/Coursemoodle.php';
 include_once '../table/Courseinfo.php';
 include_once '../config/DBClass.php';
 include_once '../lib/enrollib.php';
-include_once '../config/dbclassmoodel.php';
 require_once __DIR__ . '/../../config.php';
 require_once $CFG->dirroot . '/group/lib.php';
 require_once $CFG->dirroot . '/lib/grouplib.php';
 require_once $CFG->dirroot . '/course/externallib.php';
 
 if (
-    !($user_id = empty($_POST['user_id'])) &&
+    !($username = empty($_POST['username'])) &&
     !($group_id = empty($_POST['group_id'])) &&
     !($course_id = empty($_POST['course_id'])) &&
     !($payment = empty($_POST['payment']))
 
 ) {
-    $user_id = $_POST['user_id'];
+    $username = $_POST['username'];
+    $user_id = $DB->get_record_sql(" SELECT `id`  FROM `mdl_user` WHERE `username` = " . "'" . $username . "'");
+    if ($user_id) {
+        $user_id = $user_id->id;
+    }else{$user_id=-1;}
+
+    // $user_id = $_POST['user_id'];
     $group_id = $_POST['group_id'];
     $course_id = $_POST['course_id'];
     $payment = $_POST['payment'];
@@ -33,7 +37,7 @@ if (
 
                 if (first_payment_condition($group_id, $payment)) {
 
-                    //If the student is not already enrolled in the session add to the enroll and continue without continuing without Enrol 
+                    //If the student is not already enrolled in the session add to the enroll and continue without continuing without Enrol
                     if (!is_enroll($course_id, $user_id)) {
 
                         if (enrol_try_internal_enrol($course_id, $user_id)) {
