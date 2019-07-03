8cc781cdbbf5f65b76581ceff749fc9d927ecc81
basharhaidar <bashar.sy.1997@hotmail.com>
2019-07-03 16:02:53 +0300

    update header

--- aletqan_project/api/Students_not_completed_payments.php
+++ aletqan_project/api/Students_not_completed_payments.php
@@ -1,6 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-include_once '../table/Student_payment.php';
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");include_once '../table/Student_payment.php';
 include_once '../config/DBClass.php';
 require_once __DIR__ . '/../../config.php';
 require_once $CFG->dirroot . '/course/externallib.php';
